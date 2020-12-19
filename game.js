771bb3dd1294a9805df66cc933ccb0d64e75580c
mayura1996 <mmayurapavan@gmail.com>
2020-12-19 14:54:30 +0530

    user click-sounds enabled

--- game.js
+++ game.js
@@ -1,13 +1,28 @@
 buttonColours = ["red","blue","green","yellow"];
 gamePattern=[];
+userClickedPattern=[];
+
+$(".btn").click(function() {
+    var userChosenColour = $(this).attr("id");
+    console.log(userChosenColour);
+    userClickedPattern.push(userChosenColour);
+    console.log(userClickedPattern)
+    playSound(userChosenColour);
+ });
 
 function nextSequence(){
   randomNumber = Math.floor(Math.random()*4);
   randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
   $("#"+randomChosenColour).fadeOut(100).fadeIn(100);
-  var audio = new Audio("sounds/"+randomChosenColour+".mp3");
-  audio.play();
+  playSound(randomChosenColour);
 
 }
+
+function playSound(name){
+  var audio = new Audio("sounds/"+name+".mp3");
+  audio.play();
+}
+
+
 nextSequence();
