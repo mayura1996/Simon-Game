7c152f09eff94364fe0633322791f9562b464a94
mayura1996 <mmayurapavan@gmail.com>
2020-12-19 13:59:10 +0530

    jQuery Sounds Added

--- game.js
+++ game.js
@@ -6,5 +6,8 @@ function nextSequence(){
   randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
   $("#"+randomChosenColour).fadeOut(100).fadeIn(100);
+  var audio = new Audio("sounds/"+randomChosenColour+".mp3");
+  audio.play();
+
 }
 nextSequence();
